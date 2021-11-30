6521716774cbfa8d8dfde4e2684555a2c1bfb2ef
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-11-29 16:45:53 -0800

    Finished the lab

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,10 +1,9 @@
-menu = '''**************************************
+menuHeader = '''**************************************
 **    Welcome to the Snakes Cafe!   **
 **    Please see our menu below.    **
 **
 ** To quit at any time, type "quit" **
 **************************************
-
 Appetizers
 ----------
 Wings
@@ -30,7 +29,20 @@ Coffee
 Tea
 Unicorn Tears'''
 
-print(menu)
+menu = {
+  'Appetizers': {'wings': 0, 'cookies': 0, 'spring Rolls': 0},
+  'Entrees': {'salmon': 0, 'steak': 0, 'meat tornado': 0, 'a literal garden': 0},
+  'Desserts': {'ice Cream': 0, 'cake': 0, 'pie': 0},
+  'Drinks': {'coffee': 0, 'tea': 0, 'unicorn tears': 0}
+}
+
+for obj in menu:
+    print(obj, ' ')
+    for item in menu[obj]:
+        print(item)
+    print (' ')
+
+print(menuHeader)
 
 order_promt = '''***********************************
 ** What would you like to order? **
@@ -38,69 +50,18 @@ order_promt = '''***********************************
 print(order_promt)
 
 customer_order = input('> ').lower()
-# order_cart = ''
-# item_quantity = 0
 
 while customer_order != "quit":
-    order_cart += customer_order
-    print('** 1 order of ' + customer_order + ' have been added to your meal **')
-    item_quantity += 1
-    break
-if customer_order == order_cart:
-    print('** 2 orders of ' + customer_order + ' have been added to your meal **')
-    item_quantity += 1
-else:
-    customer_order != order_cart
-    print('** 1 order of ' + customer_order + ' have been added to your meal **')
-    item_quantity += 1
-    customer_order
-    
-print(str(item_quantity) + ' orders of ' + order_cart)
+    for key in menu.keys():
+        if customer_order in menu[key].keys():
+            menu[key][customer_order]+= 1
+            if menu[key][customer_order] == 1:
+                print(f'** {menu[key][customer_order]} order of {customer_order} has been added to your meal **')
+                break
+            else:
+                print(f'** {menu[key][customer_order]} orders of {customer_order} have been added to your meal **')
+                break
+    else:
+        print('Item not on the Menu. Please try again')
+    customer_order = input().lower()
 
-
-
-
-
-# customer_order2 = input('> ')
-# if customer_order == customer_order2:
-#     print('** 2 orders of ' + customer_order + ' have been added to your meal **')
-#     item_quantity += 1
-# else:
-#     customer_order != customer_order2
-#     print('** 1 order of ' + customer_order2 + ' have been added to your meal **')
-
-
-# customer_order3 = input('> ')
-# if customer_order == customer_order2 or customer_order3:
-#     print('** 3 orders of ' + customer_order + ' have been added to your meal **')
-#     item_quantity += 1
-# else:
-#     customer_order != customer_order2 or customer_order3
-#     print('** 1 order of ' + customer_order3 + ' have been added to your meal **')
-
-# if customer_order == customer_order2:
-#     order_cart = customer_order
-# else:
-#     customer_order != customer_order2
-#     order_cart += ' ' + customer_order2
-# if customer_order == customer_order2 or customer_order3:
-#     order_cart = ' ' + customer_order
-
-    
-
-
-
-
-# def custom_order():
-#     customer_order = input('> ')
-#     order_cart = 0
-#     for order_cart in customer_order:
-#         # print('** 1 order of ' + customer_order + ' have been added to your meal **')
-#         order_cart += customer_order:
-#         return order_cart
-#         print('** 1 order of ' + order_cart + ' have been added to your meal **')
-#     if customer_order == order_cart:
-#         print('** 2 orders of ' + customer_order + ' have been added to your meal **')
-    
-# custom_order()
-         
